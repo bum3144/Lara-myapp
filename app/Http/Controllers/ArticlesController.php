74cad65023971eedd9a096773bb77cca6f27a03d
bum3144 <bum3144@gmail.com>
2020-03-28 00:22:27 +0800

    이벤트 시스템

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -17,7 +17,7 @@ class ArticlesController extends Controller
         // $articles->load('user');
 
         // 페이지네이터 - get() 메서드를 paginate()메서드로 바꾸면 끝. (3개씩 표시)
-        $articles = \App\Article::latest()->paginate(3); // latest() 날짜 역순 => orderBy('created_at', 'desc')와 같음.
+        $articles = \App\Article::latest()->paginate(5); // latest() 날짜 역순 => orderBy('created_at', 'desc')와 같음.
 
        return view('articles.index', compact('articles'));
    }
@@ -29,31 +29,15 @@ class ArticlesController extends Controller
 
    public function store(ArticlesRequest $request)
    {
-      // $rules = [
-      //    'title' => ['required'],
-      //    'content' => ['required', 'min:10'],
-      // ];
-
-      // $messages = [
-      //    'title.required' => '제목은 필수 입력 항목입니다.',
-      //    'content.required' => '본문은 필수 입력 항목입니다.',
-      //    'content.min' => '본문은 최소 :min 글자 이상이 필요합니다.',
-      // ];
-
-      // $this->validate($request, $rules, $messages); //trait의 메서드 이용
-
-      // // trait validate()를 사용하므로 생략 가능 
-      // $validator = \Validator::make($request->all(), $rules, $messages);
-      // if ($validator->fails()){
-      //    return back()->withErrors($validator)->withInput();
-      // }
-
-      // 실제로는 auth()->user()->articles()-> ... 와 같이 현재 로그인한 사용자 인스턴트를 이용한다
       $article = \App\User::find(1)->articles()->create($request->all());
 
       if(! $article) {
          return back()->with('flash_message', '글이 저장되지 않았습니다.')->withInput();
       }
+
+      //var_dump('이벤트를 던집니다');
+      event(new \App\Events\ArticlesEvent ($article));
+      //var_dump('이벤트를 던졌습니다.');
 
       return redirect(route('articles.index'))->with('flash_message', '작성하신 글이 저장되었습니다.');
    }
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,17 @@
 
 Auth::routes();
 
-// Route::get('/', 'WelcomeController@index');
+Route::get('/', 'WelcomeController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('articles', 'ArticlesController');
+
+// 이벤트 레지스트리 (app/Providers/EventServiceProvider.php) 파일로 옮김.
+// Event::listen('article.created', function ($article){
+//     var_dump('이벤트를 받았습니다. 받은 데이터(상태)는 다음과 같습니다');
+//     var_dump($article->toArray());
+// });
 
 // DB::listen()은 이벤트 리스너다. 데이터베이스에 이벤트가 발생할 때 이벤트를 던진다.
 // DB::listen(function ($query) {
