4167ea621c6c660d367c9816607d13db4d884bc4
bum3144 <bum3144@gmail.com>
2020-04-08 19:10:46 +0800

    UI 프런트엔드 수정

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,6 +9,14 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .version();
+
+mix.scripts([
+   '../../../node_modules/highlight.js/lib/highlight.js',
+   '../../../public/js/app.js'
+], 'public/js/app.js');
+
+   
+mix.browserSync({proxy: 'localhost:8000'});
